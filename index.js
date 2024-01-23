5cf343e27de69e4b78e1034bb16a7ca096f40d14
noload <vaibhavkamble023@gmail.com>
2024-01-23 23:54:14 +0530

    added notificationTicket model

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const { sendBasicEmail } = require("./src/services/email-service");
 
 const { PORT } = require("./src/config/serverConfig");
+
+const cron = require("node-cron");
+
 const setupAndStartService = () => {
   const app = express();
 
@@ -11,12 +14,16 @@ const setupAndStartService = () => {
 
   app.listen(PORT, () => {
     console.log("server started at ", PORT);
-    sendBasicEmail(
-      "suport@admin.com",
-      "vaibhav321k321@gmail.com",
-      "this is testing mail2",
-      "hey how are you bro here we are"
-    );
+    // sendBasicEmail(
+    //   "suport@admin.com",
+    //   "vaibhav321k321@gmail.com",
+    //   "this is testing mail2",
+    //   "hey how are you bro here we are"
+    // );
+
+    cron.schedule("* * * * *", () => {
+      console.log("running a task every minute");
+    });
   });
 };
 
